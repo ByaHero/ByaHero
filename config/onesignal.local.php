98b432f649d3717ce638772fa1565a6daf957faa
timbibat <timothybibat654@gmail.com>
2026-04-04 22:55:36 +0800

    key

--- config/onesignal.local.php
+++ config/onesignal.local.php
@@ -15,5 +15,5 @@
  *                    (Use the "REST API Key", not the Safari or user-auth keys)
  */
 
-define('ONESIGNAL_APP_ID',      'your-onesignal-app-id-here');
-define('ONESIGNAL_REST_API_KEY', 'your-onesignal-rest-api-key-here');
+define('ONESIGNAL_APP_ID',      'b755dd29-1de2-4cf1-9381-6a9b436bc049');
+define('ONESIGNAL_REST_API_KEY', 'os_v2_app_w5k52ki54jgpde4bnknug26ajffmpqdyhshutleosxotea2neg6pcnw6lqotnv67mcb7p3rr3d37pglprqyefcfihmdnqxbijny3pzi');
